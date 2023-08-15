793d219090b35f46b64cde5eb9103691d2d32dbb
miscodisco <mia.jacobsen@live.dk>
2023-08-15 11:07:42 +0200

    new evaluation script with outputs saved

--- evaluation.py
+++ evaluation.py
@@ -5,27 +5,37 @@ import pandas as pd
 from sklearn.metrics import classification_report
 
 
-def print_report(data: pd.DataFrame, column: str) -> None:
+def get_report(data: pd.DataFrame, column: str):
     data = data.loc[data['train_test_set'] == "test"]
-    test_report = classification_report(data[column], data[f"pred_{column}"])
-    report_width = len(test_report.split("\n")[0])
-    
-    print("".center(report_width, "-"))
-    print("Test set only".center(report_width, " "))
-    print("".center(report_width, "-"))
-    print(test_report)
-    print("\n")
+    test_report = classification_report(data[column], data[f"pred_{column}"],output_dict=True)
+
+    return test_report
+
+
+def save_report(test_report):
+    df = pd.DataFrame(test_report).transpose()
+    df = df.assign(models = model, tasks = task, columns = column)
+
+    return df
 
 def main():
     files = glob("predictions/*_pred*.csv")
     files = [file for file in files if "only-political" not in file]
+
+    outputs = pd.DataFrame()
 
     for file in files:
         data = pd.read_csv(file)
         task, _, column, model = str(Path(file).stem).split("_")
         print(f"Model: {model}. Task: {task}. Outcome Variable: {column}")
 
-        print_report(data, column)
+        test_report = get_report(data, column)
+        
+        df = save_report
+
+        outputs = pd.concat([outputs, df])
+
+    outputs.to_csv('tweet_classification_outputs.csv')
 
 
 if __name__ == "__main__":
