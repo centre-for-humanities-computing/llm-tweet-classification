7dc09746305786a38bfef90224579bcf8c93972e
Márton Kardos <power.up1163@gmail.com>
2023-10-02 10:47:00 +0200

    Supervised classification doing less work and stratified k-fold

--- supervised_classification.py
+++ supervised_classification.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 import numpy as np
@@ -5,11 +6,11 @@ import pandas as pd
 from embetter.text import GensimEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegressionCV
-from sklearn.metrics import f1_score, recall_score, precision_score, make_scorer
-from sklearn.model_selection import cross_validate
-from sklearn.model_selection import ShuffleSplit
+from sklearn.metrics import classification_report
+from sklearn.model_selection import StratifiedKFold
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.preprocessing import StandardScaler
+from tqdm import tqdm
 
 from utils.finetune import BertFinetuneClassifier
 
@@ -38,39 +39,48 @@ def main(seed: int = 0):
 
     print("Loading data.")
     data = pd.read_csv("labelled_data.csv")
-    cv = ShuffleSplit(n_splits=5, test_size=0.2, random_state=seed)
-    scoring = ["accuracy", f1_score, recall_score, precision_score]
+    n_folds = 5
+    cross_validator = StratifiedKFold(
+        n_splits=n_folds, random_state=seed, shuffle=True
+    )
 
     out_dir = Path("output")
     out_dir.mkdir(exist_ok=True)
 
+    records = []
     for outcome in ["political", "exemplar"]:
         for model in pipelines.keys():
             print(f"Supervised classification with {model} over {outcome}")
             classifier = pipelines[model]()
-
-            X = data["raw_text"]
-            y = data[outcome]
-
-            print("initializing scorer")
-
-            all_scores = pd.DataFrame()
-            for score in scoring:
-                print(f"doing the cv for score: {score}")
-
-                if score == "accuracy":
-                    cv_scores = cross_validate(classifier, X, y, scoring=score, cv=cv)
-                else:
-                    custom_scorer = make_scorer(score, pos_label=outcome)
-                    cv_scores = cross_validate(classifier, X, y, scoring=custom_scorer, cv=cv)
-
-                all_scores[str(score)] = cv_scores['test_score']
-
-            out_path = out_dir.joinpath(f"cv_scores_{outcome}_{model}.csv")
-
-            print("saving the output")
-            all_scores.to_csv(out_path)
-
+            # Turning into numpy arrays so nothing strange happens with
+            # the indices
+            X = np.array(data["raw_text"])
+            y = np.array(data[outcome])
+            # Initiating cross validation folds
+            folds = cross_validator.split(X, y)
+            folds = tqdm(folds, total=n_folds, desc="Cross validating...")
+            for i_fold, (train_index, test_index) in enumerate(folds):
+                classifier.fit(X[train_index], y[train_index])
+                y_pred = classifier.predict(X[test_index])
+                report = classification_report(
+                    y[test_index], y_pred, output_dict=True
+                )
+                # The positive label is the same as the column name.
+                # But THIS MIGHT CHANGE so beware that then
+                # we have to relax the assumption in the next line.
+                pos_label = outcome
+                record = {
+                    "model": model,
+                    "outcome": outcome,
+                    "fold": i_fold,
+                    "accuracy": report["accuracy"],
+                    **report[pos_label],
+                }
+                records.append(record)
+    print("Saving evaluation results")
+    pd.DataFrame.from_records(records).to_csv(
+        out_dir.joinpath("cv_scores_supervised.csv")
+    )
     print("DUN")
 
 
